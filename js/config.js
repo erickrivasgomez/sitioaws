e9df78e121a22ba17ba10d591e88029dbd4f88f5
erivas <erivas@sciodev.com>
2021-05-14 11:46:00 -0500

    Added config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_aaboYkluu', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3j6kaihktqpag4q41d7t86pmqr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' ,
+    }
+};
